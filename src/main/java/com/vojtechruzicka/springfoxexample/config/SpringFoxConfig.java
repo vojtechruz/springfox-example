c1ae659952137369072cdd764a288446fbc46977
Vojtěch Růžička <vojtech.ruz@gmail.com>
2018-02-14 18:26:08 +0100

    Fixed formatting

--- src/main/java/com/vojtechruzicka/springfoxexample/config/SpringFoxConfig.java
+++ src/main/java/com/vojtechruzicka/springfoxexample/config/SpringFoxConfig.java
@@ -16,26 +16,27 @@ import java.util.Collections;
 @EnableSwagger2
 public class SpringFoxConfig {
 
-@Bean
-public Docket apiDocket(){
-    return new Docket(DocumentationType.SWAGGER_2)
-            .select()
-            .apis(RequestHandlerSelectors.any())
-            .paths(PathSelectors.any())
-            .build()
-            .apiInfo(getApiInfo());
-}
-private ApiInfo getApiInfo(){
-    return new ApiInfo(
-            "SpringFox Demo Application",
-            "This application demonstrates documenting os Spring Boot app with Swagger using SpringFox.",
-            "1.0.0",
-            "TERMS OF SERVICE URL",
-            new Contact("Vojtech Ruzicka","http://www.vojtechruzicka.com","vojtech.ruz@gmail.com"),
-            "MIT License",
-            "LICENSE URL",
-            Collections.emptyList()
-    );
-}
+    @Bean
+    public Docket apiDocket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(getApiInfo());
+    }
+
+    private ApiInfo getApiInfo() {
+        return new ApiInfo(
+                "SpringFox Demo Application",
+                "This application demonstrates documenting os Spring Boot app with Swagger using SpringFox.",
+                "1.0.0",
+                "TERMS OF SERVICE URL",
+                new Contact("Vojtech Ruzicka", "http://www.vojtechruzicka.com", "vojtech.ruz@gmail.com"),
+                "MIT License",
+                "LICENSE URL",
+                Collections.emptyList()
+        );
+    }
 
 }
