042fcac9c271d05e79c87b7e5700eee23111a086
Vojtěch Růžička <vojtech.ruz@gmail.com>
2018-02-15 20:55:27 +0100

    Added method level description using swagger core annotations for Person Controller

--- src/main/java/com/vojtechruzicka/springfoxexample/controllers/PersonController.java
+++ src/main/java/com/vojtechruzicka/springfoxexample/controllers/PersonController.java
@@ -2,6 +2,8 @@ package com.vojtechruzicka.springfoxexample.controllers;
 
 import com.vojtechruzicka.springfoxexample.domain.Person;
 import com.vojtechruzicka.springfoxexample.services.PersonService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,26 +12,31 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/v2/persons/")
+@Api(description = "Set of endpoints for Creating, Retrieving, Updating and Deleting of Persons.")
 public class PersonController {
 
     private PersonService personService;
 
-    @RequestMapping(method = RequestMethod.GET)
+    @RequestMapping(method = RequestMethod.GET, produces = "application/json")
+    @ApiOperation("Returns list of all Persons in the system.")
     public List<Person> getAllPersons() {
         return personService.getAllPersons();
     }
 
-    @RequestMapping(method = RequestMethod.GET, path = "/{id}")
+    @RequestMapping(method = RequestMethod.GET, path = "/{id}", produces = "application/json")
+    @ApiOperation("Returns a specific person by their identifier. 404 if does not exist.")
     public Person getPersonById(@PathVariable int id) {
         return personService.getPersonById(id);
     }
 
     @RequestMapping(method = RequestMethod.DELETE, path = "/{id}")
+    @ApiOperation("Deletes a person from the system. 404 if the person's identifier is not found.")
     public void deletePerson(@PathVariable int id) {
         personService.deletePerson(id);
     }
 
-    @RequestMapping(method = RequestMethod.POST)
+    @RequestMapping(method = RequestMethod.POST, produces = "application/json")
+    @ApiOperation("Creates a new person.")
     public Person createPerson(@RequestBody Person person) {
         return personService.createPerson(person);
     }
